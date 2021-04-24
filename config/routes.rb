fb825a4f43c4534c6d990892d990712330710716
Daniella Mogilner <daniella.m@hotmail.co.uk>
2021-04-24 14:49:06 +0100

    changed code of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, except: [:update, :destroy, :edit] do
-    resources :reviews, only: [:new, :create, :show]
+    resources :reviews, only: [:new, :create]
   end
+  resources :reviews, only: [:show]
 end
